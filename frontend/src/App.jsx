9e826f740387efe8451369cf6c671ee571c348b9
uc02 <upendrachauhan0206@gmail.com>
2025-04-13 13:03:01 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from react-router-dom
+import Login from './pages/Auth/login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
 
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Root/>}></Route>
+          <Route path='/login' exact element={<Login/>}></Route>
+          <Route path='/signup' exact element={<SignUp/>}></Route>
+          <Route path='/Dashboard' exact element={<Home/>}></Route>
+          <Route path='/income' exact element={<Income/>}></Route>
+          <Route path='/income' exact element={<Expense/>}></Route>
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
 export default App
+
+const Root  = () => {
+  // check if the token exists in localstorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard"/>
+  ) : (
+    <Navigate to="/login"/>
+  )
+}
