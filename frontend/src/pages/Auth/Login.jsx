106570cef7e954ed3f47074f85c8f21221b8bc6b
uc02 <upendrachauhan0206@gmail.com>
2025-04-15 09:18:54 +0530

    index.css

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Authlayout from '../../components/layouts/Authlayout'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import Input from '../../components/Input/Input';
 
 const Login = () => {
 
@@ -29,9 +30,31 @@ const Login = () => {
            value={email}
            onChange={({target}) => setEmail(target.value)}
            label="Email Address"
-           placeholder="jay@example.com"
+           placeholder="YourName@example.com"
            type='text'
            ></Input>
+
+          <Input
+           value={password}
+           onChange={({target}) => setPassword(target.value)}
+           label="Password"
+           placeholder="Min 8 characters"
+           type='password'
+           ></Input>
+
+           {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+           <button type="submit" className='btn-primary'>
+            LOGIN
+           </button>
+
+           <p className='text-[13px] text-slate-800 mt-3'>
+            Don't have an account?{" "}
+            <Link className='font-medium text-primary underline' to='/signup'>
+              SignUp 
+            </Link>
+           </p>
+
         </form>
       </div>
     </Authlayout>
